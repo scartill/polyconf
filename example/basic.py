0406e5ad5169d536031b40ea48868d79f3384cc2
Boris Resnick <boris.resnick@gmail.com>
2025-04-27 15:26:11 +0300

    CLI substitution

--- example/basic.py
+++ example/basic.py
@@ -4,55 +4,60 @@ import polyconf as pc
 schema = vo.Schema({
     vo.Required('person'): vo.Schema({
         vo.Required('name'): str,
-        vo.Required('age'): vo.All(int, vo.Range(min=0, max=120)),
+        vo.Required('score'): float
     }),
 })
 
 config = pc.Configurator(schema)
 
 # Basic dict
-config.load({'person': {'name': 'John', 'age': 30}}, format='dict')
-print(config.person.name)
+config.load({'person': {'name': 'John', 'score': 30.0}}, format='dict')
+print('Value from dict:', config.person.name)
 
 # INI
 ini = '''
 [person]
 name = John
-age = 30
+score = 30.0
 '''
 
 config.load(ini, format='ini')
-print(config.person.name)
+print('Value from INI:', config.person.name)
 
 # YAML
 yaml = '''
 person:
   name: John
-  age: 30
+  score: 30.0
 '''
 
 config.load(yaml, format='yaml')
-print(config.person.name)
+print('Value from YAML:', config.person.name)
 
 # JSON
 json = '''
 {
     "person": {
         "name": "John",
-        "age": 30
+        "score": 30.0
     }
 }
 '''
 
 config.load(json, format='json')
-print(config.person.name)
+print('Value from JSON:', config.person.name)
 
 # TOML
 toml = '''
 [person]
 name = "John"
-age = 30
+score = 30.0
 '''
 
 config.load(toml, format='toml')
-print(config.person.name)
+print('Value from TOML:', config.person.name)
+
+config.process_args()
+print('Value from CLI:', config.person.name)
+
+print("Don't forget to try with --help")
--- src/polyconf/__init__.py
+++ src/polyconf/__init__.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 from typing import Literal
+from argparse import ArgumentParser
 
 from benedict import benedict
 import voluptuous as vol
+import voluptuous.schema_builder as vsc
 
 type Format = Literal['auto', 'yaml', 'json', 'dict', 'toml', 'ini']
 
@@ -11,8 +13,16 @@ class Configurator:
     def __init__(self, schema: vol.Schema):
         self.schema = schema
         self.config = None
+        self.params = []
+        self._collect_params([], self.params, self.schema.schema)
 
-    def load(self, config_source: Path | str | dict | benedict, format: Format = 'auto'):
+    def load(
+        self, config_source: Path | str | dict | benedict,
+        *,
+        format: Format = 'auto',
+        bypass_validation: bool = False,
+        use_cli_args: bool = True
+    ):
         if format == 'auto':
             if isinstance(config_source, dict) or isinstance(config_source, benedict):
                 raise ValueError('Cannot specify "auto" format for dict input')
@@ -45,7 +55,11 @@ class Configurator:
             case _:
                 raise ValueError(f"Unsupported format specification: {format}")
 
-        self.config = self.schema(config)
+        if not bypass_validation:
+            self.config = self.schema(config)
+
+        if use_cli_args:
+            self.process_args()
 
     def __getattr__(self, item: str):
         if self.config is None:
@@ -58,3 +72,43 @@ class Configurator:
             raise AttributeError('Configuration is not loaded')
 
         return item in self.config
+
+    def _collect_params(self, prefix: list[str], params: list[tuple[list[str], type]], schema):
+        for item_name, item in schema.items():
+            if isinstance(item, vsc.Required):
+                self._collect_params(prefix, params, item.schema)
+            elif isinstance(item, vsc.Schema):
+                self._collect_params(prefix + [item_name], params, item.schema)
+            elif item is int:
+                params.append((prefix + [item_name], int))
+            elif item is str:
+                params.append((prefix + [item_name], str))
+            elif item is bool:
+                params.append((prefix + [item_name], bool))
+            elif item is float:
+                params.append((prefix + [item_name], float))
+            else:
+                raise ValueError(f'[{prefix}] Unknown schema item {item} type {type(item)}')
+
+    def parser(self):
+        parser = ArgumentParser()
+
+        for prefix, param_type in self.params:
+            param_name = '-'.join(map(str, prefix))
+            parser.add_argument(f'--{param_name}', type=param_type)
+
+        return parser
+
+    def process_args(self):
+        if self.config is None:
+            raise AttributeError('Configuration is not loaded')
+
+        parser = self.parser()
+        args = parser.parse_args()
+
+        for prefix, _ in self.params:
+            arg_param_name = '_'.join(map(str, prefix))
+            config_param_name = '.'.join(map(str, prefix))
+
+            if args_value := getattr(args, arg_param_name):
+                self.config[config_param_name] = args_value
